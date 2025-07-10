a3db8c960181816a437c586012ed318684377675
jay <jay@eximbay.com>
2025-07-10 16:47:06 +0900

    [jay-test2:feat] AI 코드 리뷰 기능 테스트

--- api/product-api/src/main/java/api/product/ProductApi.java
+++ api/product-api/src/main/java/api/product/ProductApi.java
@@ -1,4 +1,13 @@
 package api.product;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductApi {
+
+    @GetMapping("/asdfawec")
+    public String asdfawec() {
+        return "asdfawec";
+    }
 }
