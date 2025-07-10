9f9bf1ec3ff51e3a7fa8caae030cd29d9c44b531
jay <jay@eximbay.com>
2025-07-10 16:36:45 +0900

    [jay-12:feat] AI 코드 리뷰 기능 테스트

--- api/product-api/src/main/java/api/product/ProductApi.java
+++ api/product-api/src/main/java/api/product/ProductApi.java
@@ -1,4 +1,14 @@
 package api.product;
 
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductApi {
+
+    @GetMapping("/awefacewsd")
+    public String foo() {
+        return "foo";
+    }
 }
