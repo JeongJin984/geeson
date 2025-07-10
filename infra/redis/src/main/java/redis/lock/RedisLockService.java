05aa890f3aff4bb3e9b555abf2fc715528958c6a
soyeon <ryumirr1@naver.com>
2025-07-10 16:24:47 +0900

    fix:커멘트 삭제된 부분 추가하다

--- infra/redis/src/main/java/redis/lock/RedisLockService.java
+++ infra/redis/src/main/java/redis/lock/RedisLockService.java
@@ -17,14 +17,25 @@ public class RedisLockService {
     private RedissonClient redissonClient;
 
     /**
-     *  Spring DI 안되는 외부라이브러리 -> 테스트 위해 생성자를 명시적으로 작성
+     * Spring DI 안되는 외부라이브러리 -> 테스트 위해 생성자를 명시적으로 작성
+     * 
      * @param redissonClient
      */
     public RedisLockService(RedissonClient redissonClient) {
         this.redissonClient = redissonClient;
     }
 
-    public boolean doSomethingWithLock(String key, int waitTime, int leaseTime, TimeUnit unit) throws InterruptedException {
+    /**
+     * 분산락을 수행하며 waitTime과 leaseTime을 설정합니다.
+     *
+     * @param key       락의 key
+     * @param waitTime  락을 기다리는 최대 시간
+     * @param leaseTime 락을 점유하는 시간
+     * @param unit  시간 단위
+     * @return
+     */
+    public boolean doSomethingWithLock(String key, int waitTime, int leaseTime, TimeUnit unit)
+            throws InterruptedException {
         RLock lock = redissonClient.getLock("lock:" + key);
         boolean acquired = lock.tryLock(waitTime, leaseTime, unit);
         if (!acquired) {
