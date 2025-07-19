561116be178399816d437edfa4b8e773c210bac8
jay <philipwq3963@gmail.com>
2025-07-19 14:57:35 +0900

    [feat:common] url에 api version 추가

--- api/product-api/src/main/java/api/product/controller/BrandApi.java
+++ api/product-api/src/main/java/api/product/controller/BrandApi.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/brands")
+@RequestMapping("/api/v1/brands")
 @RequiredArgsConstructor
 public class BrandApi {
 
