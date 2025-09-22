b9190df27da9ddb353bf1e121cbd53970dec2759
soyeon <ryumirr1@naver.com>
2025-09-22 09:44:00 +0900

    [soy-17:feat]Inventory grpc메소드 복수 메소드 추가하다

--- infra/rdb/inventory-db/src/main/java/storage/rdb/inventory/repository/adapter/SpringDataInventoryJpaRepository.java
+++ infra/rdb/inventory-db/src/main/java/storage/rdb/inventory/repository/adapter/SpringDataInventoryJpaRepository.java
@@ -6,8 +6,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SpringDataInventoryJpaRepository extends JpaRepository<InventoryJpaEntity, Long> {
+    
     @Query("select i from InventoryJpaEntity i inner join i.product p where p.productId = :productId")
     List<InventoryJpaEntity> findAllByProductId(Long productId);
+
+    // ✅ 여러 productId 조회
+    @Query("select i from InventoryJpaEntity i inner join i.product p where p.productId in :productIds")
+    List<InventoryJpaEntity> findAllByProductIdIn(List<Long> productIds);
+
+    @Query("select i from InventoryJpaEntity i " +
+           "where i.product.productId = :productId and i.warehouseId = :warehouseId")
+    Optional<InventoryJpaEntity> findByProductIdAndWarehouseId(Long productId, Long warehouseId);
+
+    @Query("""
+        select i from InventoryJpaEntity i 
+        where (i.product.productId, i.warehouseId) in :pairs
+    """)
+    List<InventoryJpaEntity> findAllByProductIdAndWarehouseIdIn(List<Object[]> pairs);
 }
