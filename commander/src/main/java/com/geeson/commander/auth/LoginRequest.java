027425a285f8c8878b7ff9c4ffd5abc14b89b1aa
jeongjin984 <philipwq3963@gmail.com>
2025-11-30 13:15:22 +0900

    [chore:db] ddl script repository 이관

--- commander/src/main/java/com/geeson/commander/auth/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.geeson.commander.auth;
-
-public record LoginRequest(
-    String username,
-    String password
-) {
-}
--- commander/src/main/java/com/geeson/commander/auth/LoginResponse.java
+++
@@ -1,7 +0,0 @@
-package com.geeson.commander.auth;
-
-public record LoginResponse(
-    String token,
-    String username
-) {
-}
